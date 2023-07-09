490f783de3ac22da0402e40830333ef72dfda5fe
Jacob Silberstein <jake.silberstein1@gmail.com>
2023-07-09 17:58:30 -0400

    Use MVC pattern to manage application logic

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ctrlMain = require('.../controllers/main');
+const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
