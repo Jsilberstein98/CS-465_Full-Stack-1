589f78a3c57231d9297ec05cff8512797e80bbb6
Jacob Silberstein <jake.silberstein1@gmail.com>
2023-08-11 14:45:15 -0400

    Added delete-trip

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -10,8 +10,9 @@ router
 
 
 router
-    .route('./trips/:tripCode')
+    .route('/trips/:tripCode')
     .get(tripsController.tripsFindByCode)
-    .put(tripsController.tripsUpdateTrip);
+    .put(tripsController.tripsUpdateTrip)
+    .delete(tripsController.tripsDeleteTrip);
         
 module.exports = router;
